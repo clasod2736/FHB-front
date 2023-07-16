6b41cb3d7abddadcfccf11b26e150d198de5fd8c
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2023-07-16 21:36:07 +1000

    finish component

--- homebrewing/src/components/Finish/Finish.jsx
+++ homebrewing/src/components/Finish/Finish.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
 import './Finish.css'
 
-import FinalLogo from './FinalLogo/FinalLogo'
+// import {ReactComponent as FinalLogo} from '../../assets/finalLogo2.svg'
 
 export default function Finish() {
   return (
     <div className='finishContainer'>
         <div className='finishContents'>
-            {/* <svg width={'85%'} height={'65%'} viewBox='25 230 450 100'> */}
-                <FinalLogo/>
-            {/* </svg> */}
-            <div className='btnContainer'>
-                <button id='favourite'>Save This Coffee.</button>
-                /
-                <button id='restart'>Try different!</button>
-                /
-                <button id='shop'>Want to buy Something?</button>
+          <div className='finishText'>
+            <header>How was your Coffee today?</header>
+            <div className='pContainer'>
+              <p>It is a long established fact that a reader will be distracted</p>
+              <p>it look like readable English. Many desktop</p>
+              <p>publishing packages and web page</p>
             </div>
+            <div className='btnContainer'>
+              <button className='recipe'>Save Recipe</button>
+              <button className='coffee'>Try Another</button>
+              <button className='shop'>Explore Shop</button>
+            </div>
+          </div>
         </div>
     </div>
   )
--- homebrewing/src/components/Intro/Intro.jsx
+++ homebrewing/src/components/Intro/Intro.jsx
@@ -8,19 +8,20 @@ export default function Intro() {
   return (
     <div className='introContainer'>
       <div className='logoContainer'>
-      <div className='gate'>
-          <Logo/>
+        <div className='gate'>
+          {/* <Logo/> */}
+        <img src="/img/background/logo.jpg" alt="logo" />
           <div className='pContainer'>
             <p className='introduce1'>Great Coffee could be powerful inspiration generator.</p>
             <p className='introduce2'>This web experience is designed for everyone who want to making a great Coffee at home.</p>
             <p className='introduce3'>Enjoy my recommendations and make a fresh start in your days!</p>
           </div>
-        <div className='expContatiner'>
-          <Link to='/login' className='exploreBtn'>
-            Let's Brew!
-          </Link>
+          <div className='expContatiner'>
+            <Link to='/login' className='exploreBtn'>
+              Let's Brew!
+            </Link>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   )
