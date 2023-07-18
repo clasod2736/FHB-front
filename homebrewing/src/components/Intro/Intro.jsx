45fe9e4a051c4b6065354b992a9a6a2bb79b465c
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2023-07-18 19:54:22 +1000

    login basic function

--- homebrewing/src/components/Intro/Intro.jsx
+++ homebrewing/src/components/Intro/Intro.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 import './Intro.css'
 import Logo from './Logo/NewLogo'
 
-import { Link } from 'react-router-dom'
+import { Link,useParams } from 'react-router-dom'
+
+import { useSelector } from 'react-redux'
 
 export default function Intro() {
+  const isLogin = useSelector((state) => state.login);
+
+  const {userName} = useParams();
 
   return (
     <div className='introContainer'>
@@ -15,7 +20,7 @@ export default function Intro() {
             <Link to='/login' className='exploreBtn'>
               Make Recent Brew!
             </Link>
-            <Link to='/login' className='exploreBtn'>
+            <Link to={isLogin ? `/${userName}/menu` : '/login'} className='exploreBtn'>
               Make a New Brew!
             </Link>
           </div>
