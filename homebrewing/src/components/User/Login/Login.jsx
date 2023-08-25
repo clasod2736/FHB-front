ae2893b328f2395c2e27823c85338be1eca94df1
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2023-08-25 14:42:02 +1000

    test commit

--- homebrewing/src/components/User/Login/Login.jsx
+++ homebrewing/src/components/User/Login/Login.jsx
@@ -26,7 +26,8 @@ export default function Login() {
                 params: {
                     email: email,
                     password: password
-            }
+            },
+            withCredentials: true
         }, )
 
         if (typeof response !== "string") {
