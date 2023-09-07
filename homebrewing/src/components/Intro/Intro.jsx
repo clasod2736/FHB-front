e1dab070dd16a4b85001a7a70c16fbaaad998941
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2023-09-07 11:00:03 +1000

    Fix useEffect in Brewing component that fetchData from DB and remove extra useEffect

--- homebrewing/src/components/Intro/Intro.jsx
+++ homebrewing/src/components/Intro/Intro.jsx
@@ -1,94 +1,88 @@
-import React, {useState, useEffect} from 'react'
-import './Intro.css'
-import Logo from './Logo/NewLogo'
+import React, { useState, useEffect } from "react";
+import "./Intro.css";
+import Logo from "./Logo/NewLogo";
 
-import { Link, useNavigate } from 'react-router-dom'
-import axios from 'axios'
-import { useSelector } from 'react-redux'
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 export default function Intro() {
   const isLogIn = useSelector((state) => state.logIn);
   const userEmail = useSelector((state) => state.userEmail);
-  const [ oldBrews, setOldBrews ] = useState([])
-  const [ recentBrew, setRecentBrew ] = useState(null);
+  const [recentBrew, setRecentBrew] = useState(null);
 
   const navigate = useNavigate();
 
-//fetch data for get recent brew data.
+  //fetch data for get recent brew data.
   useEffect(() => {
-    
     if (isLogIn === true) {
-      
-      fetchDatas();
-
       async function fetchDatas() {
-          const serverUrl = 'http://localhost:8080/getOldbrews'
-  
-          try {
-              const response = await axios.get(serverUrl, {
-                  params: {
-                      email: userEmail
-                  }
-              })
-              setOldBrews(response.data)
+        const serverUrl = "http://localhost:8080/getOldbrews";
 
-              //state and sort recent brew data.
-              const sortedBrews = oldBrews.sort((a, b) => b.order - a.order);
-              setRecentBrew(sortedBrews[0]);
-          }
-          catch (error) {
-              console.log(error)
-          }
+        try {
+          const response = await axios.get(serverUrl, {
+            params: {
+              email: userEmail,
+            },
+          });
+          const fetchedBrews = response.data;
+
+          //state and sort recent brew data.
+          const sortedBrews = fetchedBrews.sort((a, b) => b.order - a.order);
+          setRecentBrew(sortedBrews[0]);
+        } catch (error) {
+          console.log(error);
+        }
       }
-    } else if(isLogIn === false){ return }
-}, [isLogIn, oldBrews, userEmail])
+      fetchDatas();
+    } else if (isLogIn === false) {
+      setRecentBrew(null); // Set recentBrew to null
+    }
+  }, [isLogIn, userEmail]);
 
-//set recentBrew
-useEffect(()=> {
-  const sortedBrews = oldBrews.sort((a, b) => b.order - a.order);
-  setRecentBrew(sortedBrews[0]);
-}, [oldBrews])
-
-//Set right Buttons with loggedIn or not
-const settingButtons  = () => {
-  if (isLogIn === false ) {
-    return (
-      <div className='loginContainer'>
-        <Link className='login' to={'/login'}>
-          Login!
-        </Link>
-      </div>
-    )
-  } else {
-    return (
-      <div className='expContainer'>
-        <button className='exploreBtn'
-        onClick={() => {
-          if (isLogIn) {
-            recentBrew !== null ? 
-            navigate(`/menu/${recentBrew.menuName}/method/${recentBrew.methodName}/recipe/${recentBrew.water}/${recentBrew.coffee}/${recentBrew.roasting}/${recentBrew.grind}/brewing`)
-            :
-            alert("You don't have Any History...make a New Brew!")
-          }
-        }}>
-          Make Last Brew!
-        </button>
-        <Link to={isLogIn ? '/menu' : '/login'} className='exploreBtn'>
-          Make New Brew!
-        </Link>
-      </div>
-    )
-  }
-}
+  //Set right Buttons with loggedIn or not
+  const settingButtons = () => {
+    if (isLogIn === false) {
+      return (
+        <div className="loginContainer">
+          <Link className="login" to={"/login"}>
+            Login!
+          </Link>
+        </div>
+      );
+    } else {
+      return (
+        <div className="expContainer">
+          <button
+            className="exploreBtn"
+            onClick={() => {
+              if (isLogIn) {
+                recentBrew !== null
+                  ? navigate(
+                      `/menu/${recentBrew.menuName}/method/${recentBrew.methodName}/recipe/${recentBrew.water}/${recentBrew.coffee}/${recentBrew.roasting}/${recentBrew.grind}/brewing`
+                    )
+                  : alert("You don't have Any History...make a New Brew!");
+              }
+            }}
+          >
+            Make Last Brew!
+          </button>
+          <Link to={isLogIn ? "/menu" : "/login"} className="exploreBtn">
+            Make New Brew!
+          </Link>
+        </div>
+      );
+    }
+  };
 
   return (
-    <div className='introContainer'>
-      <div className='logoContainer'>
-        <div className='gate'>
-          <Logo/>
+    <div className="introContainer">
+      <div className="logoContainer">
+        <div className="gate">
+          <Logo />
           {settingButtons()}
         </div>
       </div>
     </div>
-  )
+  );
 }
