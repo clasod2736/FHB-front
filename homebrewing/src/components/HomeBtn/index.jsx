a245ebed80d79a67bc4ca06767c1f407df74bf07
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2023-09-25 19:49:31 +1000

    Added Home button only for mobile & Tap vision

---
+++ homebrewing/src/components/HomeBtn/index.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+import "./HomeBtn.css";
+
+import { useNavigate } from "react-router-dom";
+
+import { AiFillHome } from "react-icons/ai";
+
+export default function HomeBtn() {
+  const navigate = useNavigate();
+
+  const [width, setWidth] = useState(0);
+
+  //Mobile setup
+  const currentWidth = window.innerWidth;
+
+  useEffect(() => {
+    const resizeListener = () => {
+      setWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", resizeListener);
+    window.addEventListener("load", resizeListener);
+    setWidth(currentWidth);
+
+    return () => {
+      window.removeEventListener("resize", resizeListener);
+      window.removeEventListener("load", resizeListener);
+    };
+  }, [width, currentWidth]);
+
+  const showHomeBtn = width <= 1278;
+
+  return (
+    <div className="homebtn--container" style={{ display: showHomeBtn ? "flex" : "none" }}>
+      <div className="homebtn--btn">
+        <button
+          className="homeBtn"
+          onClick={() => {
+            navigate("/");
+          }}
+        >
+          <AiFillHome />
+        </button>
+      </div>
+    </div>
+  );
+}
--- homebrewing/src/pages/Root.jsx
+++ homebrewing/src/pages/Root.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import './Root.css'
-import Navbar from '../components/Navbar'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import "./Root.css";
+import Navbar from "../components/Navbar";
+import { Outlet } from "react-router-dom";
+import HomeBtn from "../components/HomeBtn";
 
 export default function Root() {
   return (
-    <div className='rootContainer'>
-      <Navbar/>
-      <Outlet/>
+    <div className="rootContainer">
+      <Navbar />
+      <Outlet />
+      <HomeBtn />
     </div>
-  )
+  );
 }
