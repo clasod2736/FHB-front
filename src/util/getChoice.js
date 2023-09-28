505865d4af0b28882527e8de9c786a2c23e6ed50
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2023-09-29 07:34:29 +1000

    fixing forEach function...

--- src/util/getChoice.js
+++ src/util/getChoice.js
@@ -1,101 +1,120 @@
 export default function getChoices(oldBrews) {
+  const menuArr = [
+    "espresso",
+    "americano",
+    "longBlack",
+    "coldBrew",
+    "latte",
+    "flatWhite",
+    "capuiccino",
+    "mocha",
+    "macchiato",
+    "chai",
+    "turmeric",
+    "icedCoffee",
+    "affogato",
+  ];
+  const methodArr = ["harioV6", "mokapot", "frenchpress", "aeropress", "chemax", "shypon"];
+  const roastingArr = ["dark", "medium", "light"];
+  const grindArr = ["coarse", "medium", "fine"];
 
-    const menuArr = ['espresso', 'americano', 'longBlack', 'coldBrew', 'latte', 'flatWhite', 'capuiccino', 'mocha', 'macchiato', 'chai', 'turmeric', 'icedCoffee', 'affogato']
-    const methodArr= ['harioV6', 'mokapot', 'frenchpress', 'aeropress', 'chemax', 'shypon']
-    const roastingArr = ['dark', 'medium', 'light']
-    const grindArr = ['coarse', 'medium', 'fine']
+  //get most frequent value with number of frequency
+  function countFrequency(data, array, option) {
+    const frequency = {};
 
-    //get most frequent value with number of frequency
-    function countFrequency(data, array, option) {
-        const frequency = {};
-
-            for (const item of data) {
-            const { menuName, methodName, roasting, grind } = item
-            if (option === 'menu') {
-                if (array.includes(menuName)) {
-                    if (frequency[menuName]) {
-                    frequency[menuName]++;
-                    } else {
-                    frequency[menuName] = 1;
-                    }
-                }
-            } else if (option === 'method') {
-                if (array.includes(methodName)) {
-                    if (frequency[methodName]) {
-                    frequency[methodName]++;
-                    } else {
-                    frequency[methodName] = 1;
-                    }
-                }
-            } else if (option === 'roasting') {
-            if (array.includes(roasting)) {
-                if (frequency[roasting]) {
-                frequency[roasting]++;
-                } else {
-                frequency[roasting] = 1;
-                }
-            }
-            } else if (option === 'grind') {
-            if (array.includes(grind)) {
-                if (frequency[grind]) {
-                frequency[grind]++;
-                } else {
-                frequency[grind] = 1;
-                }
-            }
-            }
+    for (const item of data) {
+      const { menuName, methodName, roasting, grind } = item;
+      if (option === "menu") {
+        if (array.includes(menuName)) {
+          if (frequency[menuName]) {
+            frequency[menuName]++;
+          } else {
+            frequency[menuName] = 1;
+          }
         }
-        
-        const sortedFrequency = Object.entries(frequency)
-          .sort((a, b) => b[1] - a[1]);
-        
-        const result = sortedFrequency.slice(0, 1)
-      
-        return result;
+      } else if (option === "method") {
+        if (array.includes(methodName)) {
+          if (frequency[methodName]) {
+            frequency[methodName]++;
+          } else {
+            frequency[methodName] = 1;
+          }
+        }
+      } else if (option === "roasting") {
+        if (array.includes(roasting)) {
+          if (frequency[roasting]) {
+            frequency[roasting]++;
+          } else {
+            frequency[roasting] = 1;
+          }
+        }
+      } else if (option === "grind") {
+        if (array.includes(grind)) {
+          if (frequency[grind]) {
+            frequency[grind]++;
+          } else {
+            frequency[grind] = 1;
+          }
+        }
+      }
     }
-    //get most frequent value
-    function getMostFrequentValue(data, key) {
-        const valueCount = {};
-        
-        data.forEach(item => {
-            const value = item[key];
-            if (valueCount[value]) {
-              valueCount[value]++;
-            } else {
-              valueCount[value] = 1;
-            }
-          });
 
-        const sortedCounts = Object.entries(valueCount).sort((a, b) => b[1] - a[1]);
+    const sortedFrequency = Object.entries(frequency).sort((a, b) => b[1] - a[1]);
 
-        return sortedCounts[0][0];
-    }
+    const result = sortedFrequency.slice(0, 1);
 
-    if (oldBrews.length <= 0) {
-        return "No history for calculating.."
-    } else {
-        
-        //all of functions
-        const totalBrew = oldBrews.length
-        const menuChoice =  countFrequency(oldBrews, menuArr, "menu");
-        const methodChoice = countFrequency(oldBrews, methodArr, "method");
-        const roastingChoice = countFrequency(oldBrews, roastingArr, "roasting");
-        const grindChoice = countFrequency(oldBrews, grindArr, "grind");
-        const coffeeChoice = getMostFrequentValue(oldBrews, 'coffee')
-        const waterChoice = getMostFrequentValue(oldBrews, 'water')
-        console.log(totalBrew, menuChoice, methodChoice, roastingChoice, grindChoice, coffeeChoice, waterChoice)
-        
-        //Final object.
-        const choices = {
-            totalBrew: totalBrew,
-            menu: menuChoice,
-            method: methodChoice,
-            roasting: roastingChoice,
-            grind: grindChoice,
-            coffee: coffeeChoice,
-            water: waterChoice
-          };
+    return result;
+  }
+  //get most frequent value
+  function getMostFrequentValue(data, key) {
+    const valueCount = {};
 
-        return choices
-    }
+    data.forEach((item) => {
+      const value = item[key];
+      if (valueCount[value]) {
+        valueCount[value]++;
+      } else {
+        valueCount[value] = 1;
+      }
+    });
+
+    const sortedCounts = Object.entries(valueCount).sort((a, b) => b[1] - a[1]);
+
+    return sortedCounts[0][0];
+  }
+
+  if (oldBrews.length <= 0) {
+    return "No history for calculating..";
+  } else {
+    //all of functions
+    const totalBrew = oldBrews.length;
+    const menuChoice = countFrequency(oldBrews, menuArr, "menu");
+    const methodChoice = countFrequency(oldBrews, methodArr, "method");
+    const roastingChoice = countFrequency(oldBrews, roastingArr, "roasting");
+    const grindChoice = countFrequency(oldBrews, grindArr, "grind");
+    const coffeeChoice = getMostFrequentValue(oldBrews, "coffee");
+    const waterChoice = getMostFrequentValue(oldBrews, "water");
+    console.log(
+      totalBrew,
+      menuChoice,
+      methodChoice,
+      roastingChoice,
+      grindChoice,
+      coffeeChoice,
+      waterChoice
+    );
+
+    //Final object.
+    const choices = {
+      totalBrew: totalBrew,
+      menu: menuChoice,
+      method: methodChoice,
+      roasting: roastingChoice,
+      grind: grindChoice,
+      coffee: coffeeChoice,
+      water: waterChoice,
+    };
+
+    return choices;
+  }
 }
