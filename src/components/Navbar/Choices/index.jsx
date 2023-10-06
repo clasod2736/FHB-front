6f93acc4ec14511781be1665c44bec671a3bc510
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2023-10-06 18:21:52 +1100

    choices error

--- src/components/Navbar/Choices/index.jsx
+++ src/components/Navbar/Choices/index.jsx
@@ -66,7 +66,7 @@ export default function Choices({ click }) {
         const mostChoices = getChoices(response);
         console.log(mostChoices);
         if (mostChoices <= 0) {
-          return;
+          setData(null);
         } else setData(mostChoices);
       }
       result();
