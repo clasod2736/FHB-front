9ef22fcae69a0dac0d52c246f1bb1c58b14fcd72
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2023-10-10 16:25:05 +1100

    readme

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # FHB / For-Homey-Barista.
+
 > This web-application is my First Full-Stack Project that could provide lean and nice **Coffee Brewing Guide** for user want to be barista at their own home.
-> Journey is start from *Register User* and end with check all the *Brew history* users made, between there are simple and easy brewing steps with smooth animation.
-> Through this project I understand **"How"** is data flowing from *Client-side* to *Server-side* and Database(also vice-versa).
+> Journey is start from _Register User_ and end with check all the _Brew history_ users made, between there are simple and easy brewing steps with smooth animation.
+> Through this project I understand **"How"** is data flowing from _Client-side_ to _Server-side_ and Database(also vice-versa).
 > Understanding fundermental of Component based library like the React. How to make smooth and clean animation and layout in CSS with Post-processer.
 
-![FHB Overview](https://github.com/clasod2736/FHB-front/assets/109887795/5dfb91be-7f90-447b-a4c9-b2972e95f3ac){: width="200" height="400"}
-
+![FHB Overview](https://github.com/clasod2736/FHB-front/assets/109887795/5dfb91be-7f90-447b-a4c9-b2972e95f3ac)
--- src/components/Brewing/components/Steps/index.jsx
+++ src/components/Brewing/components/Steps/index.jsx
@@ -1,89 +1,116 @@
-import React, { useEffect, useState } from 'react'
-import './Steps.css'
-import { useParams, useNavigate } from 'react-router-dom'
-import methodSteps from '../../../../util/StuffData/MethodSteps.json'
-import menuSteps from '../../../../util/StuffData/MenuSteps.json'
+import React, { useEffect, useState } from "react";
+import "./Steps.css";
+import { useParams, useNavigate } from "react-router-dom";
+import methodSteps from "../../../../util/StuffData/MethodSteps.json";
+import menuSteps from "../../../../util/StuffData/MenuSteps.json";
 
-export default function Steps({isMobile}) {
-const [methodStepInfo, setMethodStepInfo] = useState([]);
-const [menuStepInfo, setMenuStepInfo] = useState([]);
-const [steps, setSteps] = useState(undefined)
+export default function Steps({ isMobile }) {
+  const [methodStepInfo, setMethodStepInfo] = useState([]);
+  const [menuStepInfo, setMenuStepInfo] = useState([]);
+  const [steps, setSteps] = useState(undefined);
 
-const navigate = useNavigate();
-const { menuName, methodName } = useParams();
+  const navigate = useNavigate();
+  const { menuName, methodName } = useParams();
 
-//get Json data from util folder
-useEffect(() => {
-    setMethodStepInfo(methodSteps[methodName])
-    setMenuStepInfo(menuSteps[menuName])
-    isMobile ? setSteps(0) : setSteps(undefined)
-}, [methodName, menuName, isMobile])
+  //get Json data from util folder
+  useEffect(() => {
+    setMethodStepInfo(methodSteps[methodName]);
+    setMenuStepInfo(menuSteps[menuName]);
+    isMobile ? setSteps(0) : setSteps(undefined);
+  }, [methodName, menuName, isMobile]);
 
-//rendering Brewing Steps
-const brewingSteps = () => {
-    
+  //rendering Brewing Steps
+  const brewingSteps = () => {
     if (steps !== undefined && methodStepInfo.length > steps) {
+      //method Steps
+      const title = methodStepInfo[steps].step[0].title;
+      const gifUrl = methodStepInfo[steps].step[1].gif;
+      const description = methodStepInfo[steps].step[2].description;
 
-        //method Steps
-        const title = methodStepInfo[steps].step[0].title
-        const gifUrl = methodStepInfo[steps].step[1].gif
-        const description = methodStepInfo[steps].step[2].description
-
-    return (
-        <div className='methodSteps'>
-            <header>{title}</header>
-            <img src={gifUrl} alt="brewingGif"/>
-            <div className='brewingStepsText'>
-                <p>{description}</p>
-            </div>
-            <div className='btnContainer'>
-                <button onClick={() => {setSteps((prev) => prev - 1)}}
-                style={{display: steps === 0 ? 'none' : 'block'}}>Back</button>
-                <button onClick={() => {setSteps((prev) => prev + 1)}}>Next</button>
-            </div>
+      return (
+        <div className="methodSteps">
+          <header>{title}</header>
+          <img src={gifUrl} alt="brewingGif" />
+          <div className="brewingStepsText">
+            <p>{description}</p>
+          </div>
+          <div className="btnContainer">
+            <button
+              onClick={() => {
+                setSteps((prev) => prev - 1);
+              }}
+              style={{ display: steps === 0 ? "none" : "block" }}
+            >
+              Back
+            </button>
+            <button
+              onClick={() => {
+                setSteps((prev) => prev + 1);
+              }}
+            >
+              Next
+            </button>
+          </div>
         </div>
-        )
+      );
 
-    // After method Steps Show menu direction.
-    } else if ( methodStepInfo.length <= steps ) {
+      // After method Steps Show menu direction.
+    } else if (methodStepInfo.length <= steps) {
+      //menu Steps
+      const img = menuStepInfo[0].img;
+      const direction = menuStepInfo[1].direction;
+      console.log(img, direction);
 
-        //menu Steps
-        const img = menuStepInfo[0].img
-        const direction = menuStepInfo[1].direction
-        console.log(img, direction)
-
-        return (
-            <div className='menuSteps'>
-                <img src={img} alt="menuImg" />
-                <div className='menuStepContents'>
-                    <header>{menuName}</header>
-                    <div className='directions'>
-                        {direction.map((dir) => (
-                            <li key={dir}>{dir}</li>
-                        ))}
-                    </div>
-                    <div className='btnContainer'>
-                          <button onClick={() => {setSteps(0)}}>Back to Start</button>  
-                          <button onClick={() => {navigate('./finish')}}>Finish</button>
-                    </div>
-                </div>
+      return (
+        <div className="menuSteps">
+          <img src={img} alt="menuImg" />
+          <div className="menuStepContents">
+            <header>{menuName}</header>
+            <div className="directions">
+              {direction.map((dir) => (
+                <li key={dir}>{dir}</li>
+              ))}
             </div>
-        )
+            <div className="btnContainer">
+              <button
+                onClick={() => {
+                  setSteps(0);
+                }}
+              >
+                Back to Start
+              </button>
+              <button
+                onClick={() => {
+                  navigate("./finish");
+                }}
+              >
+                Finish
+              </button>
+            </div>
+          </div>
+        </div>
+      );
     }
-}
+  };
 
   return (
-    <div className='stepsContainer'>
-        <div className='steps'  style={{display: isMobile ? 'none' : 'flex'}}>
-            <div className='stepsBefore' style={{ display: steps === undefined ? "flex" : "none"}}>
-                <header>Did you check everything? Then...</header>  
-                <button onClick={() => {setSteps(0)}}>Start Brewing!</button>
-            </div>
-            {brewingSteps()}
+    <div className="stepsContainer">
+      <div className="steps" style={{ display: isMobile ? "none" : "flex" }}>
+        <div className="stepsBefore" style={{ display: steps === undefined ? "flex" : "none" }}>
+          <header>Did you check everything? Then...</header>
+          <button
+            onClick={() => {
+              setSteps(0);
+            }}
+          >
+            Start Brewing!
+          </button>
         </div>
-        <div className='stepsMobile' style={{display: isMobile ? 'flex' : 'none'}}>
-            {brewingSteps()}
-        </div>
+        {brewingSteps()}
+      </div>
+      <div className="stepsMobile" style={{ display: isMobile ? "flex" : "none" }}>
+        {brewingSteps()}
+      </div>
     </div>
-  )
+  );
 }
