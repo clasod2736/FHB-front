f31680a2e2b464731a6c04acccc678844dac3674
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2024-04-11 12:19:17 +1000

    added portfolio website and linkedin links

--- src/components/Navbar/SlideMenu/index.jsx
+++ src/components/Navbar/SlideMenu/index.jsx
@@ -51,7 +51,7 @@ export default function SlideMenu() {
               <a href="https://github.com/clasod2736">
                 <BsGithub />
               </a>
-              <a href="www.linkedin.com/in/joon-park-772a8820b">
+              <a href="https://joondev.me">
                 <TbWorld />
               </a>
             </div>
