3dbf896aa44d1ef22e76e648beac09ae4966f0d1
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2024-04-13 15:18:36 +1000

    Test

--- src/util/getPreload.js
+++ src/util/getPreload.js
@@ -7,7 +7,6 @@ const menuImgArr = Object.entries(menuArr).map(([name, data]) => ({
 }));
 
 export function preloadingMenuImg() {
-  console.log(menuImgArr);
   menuImgArr.forEach((img) => {
     const image = new Image();
     image.src = img.img;
@@ -22,16 +21,12 @@ export const preloadImg = (imgAdd) => {
 };
 
 export function gifArr(methodName) {
-  // methodName에 해당하는 객체를 찾습니다.
   const methodObj = methodSteps[methodName];
   if (!methodObj) {
-    // methodName에 해당하는 객체가 없으면 빈 배열을 반환합니다.
     return [];
   }
 
-  // methodName에 해당하는 객체의 "step" 배열을 가져와서 각 항목에서 gif 값을 추출합니다.
-  const gifs = methodObj.flatMap(({ step }) => step.map(({ gif }) => gif));
+  const gifs = methodObj.flatMap(({ step }) => step.map(({ gif }) => gif)).filter(Boolean);
 
-  // 추출된 gif 값을 가지고 있는 배열을 반환합니다.
   return gifs;
 }
